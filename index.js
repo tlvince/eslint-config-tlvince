946faff23fd077fd811373c4c8699ad74da25179
Tom Vincent <git@tlvince.com>
2015-05-23 16:52:34 +0100

    fix: remove YAML runtime dependency

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-'use strict';
-
-var fs = require('fs');
-var path = require('path');
-var yaml = require('js-yaml');
-
-var config = path.join(__dirname, 'eslintrc.yml');
-
-module.exports = yaml.safeLoad(fs.readFileSync(config, 'utf-8'));
+module.exports = require('./eslintrc.json');
