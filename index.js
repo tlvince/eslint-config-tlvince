e4c0794b5ba6d2dd77c6c6ceb5665f0852f3efdc
Tom Vincent <git@tlvince.com>
2019-10-25 18:11:09 +0100

    feat: rewrite for current typescript set up
    BREAKING CHANGE: rewritten to use typescript and prettier

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-module.exports = require('./eslintrc.json');
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+  env: {
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts'],
+      env: {
+        jest: true,
+      },
+      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+    },
+  ],
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const assert = require('assert')
+const config = require('.')
+
+assert.ok(config.rules)
