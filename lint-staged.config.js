97a9cfc478de8d8903666bbf17a51a06ff86a979
Tom Vincent <tom.vincent@clearscore.com>
2019-11-11 13:42:52 +0000

    chore(repo): add prettierrc

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "*.ts": [
-    "eslint --cache --fix",
-    "git add",
-    "yarn test --bail --findRelatedTests"
+  '*.ts': [
+    'eslint --cache --fix',
+    'git add',
+    'yarn test --bail --findRelatedTests',
   ],
-  "*.{js,ts,json,css,md}": ["prettier --write", "git add"]
-};
+  '*.{js,ts,json,css,md}': ['prettier --write', 'git add'],
+}
